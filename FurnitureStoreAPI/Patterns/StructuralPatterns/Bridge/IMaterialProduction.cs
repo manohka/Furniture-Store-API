d1f21e5b0c39bc3e673c52612acbab3df8fdb2a3
manohka <karthickmanohar95@gmail.com>
2026-01-14 20:00:39 +0530

    Added readme details to Bridge Implementation Interface

--- FurnitureStoreAPI/Patterns/StructuralPatterns/Bridge/IMaterialProduction.cs
+++ FurnitureStoreAPI/Patterns/StructuralPatterns/Bridge/IMaterialProduction.cs
@@ -1,4 +1,7 @@
-﻿namespace FurnitureStoreAPI.Patterns.StructuralPatterns.Bridge
+﻿using FurnitureStoreAPI.Models;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+
+namespace FurnitureStoreAPI.Patterns.StructuralPatterns.Bridge
 {
     // Implementation Interface - Material Production
     public interface IMaterialProduction
@@ -10,3 +13,16 @@
         string GetMaintenanceInfo();
     }
 }
+
+/*Key Bridge Pattern Benefits Here:
+
+
+No Class Explosion - No need for WoodenChair, MetalChair, etc.
+
+Flexible Combinations - 4 furniture types × 4 materials = 16 combos
+
+Runtime Changes - Change material without creating new object
+
+Independent Scaling - Add new furniture/ material easily
+
+Single Responsibility - Furniture handles type, Material handles production*/
